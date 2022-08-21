d1f0ec775b0334907b1ddb048fd0816cda552082
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-21 19:14:34 +0300

    Add comments for TransactionScope

--- Kot.MongoDB.Migrations/TransactionScope.cs
+++ Kot.MongoDB.Migrations/TransactionScope.cs
@@ -1,9 +1,23 @@
 ﻿namespace Kot.MongoDB.Migrations
 {
+    /// <summary>
+    /// Migration transaction scope.
+    /// </summary>
     public enum TransactionScope
     {
+        /// <summary>
+        /// Apply migrations without transactions.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// Apply each migration in a separate transaction.
+        /// </summary>
         SingleMigration,
+
+        /// <summary>
+        /// Apply all migrations in a single transaction.
+        /// </summary>
         AllMigrations
     }
 }
