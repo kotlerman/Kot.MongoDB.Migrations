0ebfb48ce8fadc097af87f57cfca192791f074fa
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-21 19:14:55 +0300

    Add comments for IMongoMigration

--- Kot.MongoDB.Migrations/IMongoMigration.cs
+++ Kot.MongoDB.Migrations/IMongoMigration.cs
@@ -4,11 +4,39 @@ using System.Threading.Tasks;
 
 namespace Kot.MongoDB.Migrations
 {
+    /// <summary>
+    /// Represents a Mongo migration.
+    /// </summary>
     public interface IMongoMigration
     {
+        /// <summary>
+        /// Unique migration version.
+        /// </summary>
         DatabaseVersion Version { get; }
+
+        /// <summary>
+        /// Migration name.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// An action performed when migration is applied.
+        /// </summary>
+        /// <param name="db">Mongo database.</param>
+        /// <param name="session">Client session. <see langword="null"/> when <see cref="MigrationOptions.TransactionScope"/>
+        /// is <see cref="TransactionScope.None"/>.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns><see cref="Task"/> representing Up operation.</returns>
         Task UpAsync(IMongoDatabase db, IClientSessionHandle session, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// An action performed when migration is rolled back.
+        /// </summary>
+        /// <param name="db">Mongo database.</param>
+        /// <param name="session">Client session. <see langword="null"/> when <see cref="MigrationOptions.TransactionScope"/>
+        /// is <see cref="TransactionScope.None"/>.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns><see cref="Task"/> representing Down operation.</returns>
         Task DownAsync(IMongoDatabase db, IClientSessionHandle session, CancellationToken cancellationToken);
     }
 }
