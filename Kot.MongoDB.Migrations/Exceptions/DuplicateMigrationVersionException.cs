fcee096e40607f85edf37d0b5f2954819491f96f
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-22 21:29:41 +0300

    Add comments for DuplicateMigrationVersionException

--- Kot.MongoDB.Migrations/Exceptions/DuplicateMigrationVersionException.cs
+++ Kot.MongoDB.Migrations/Exceptions/DuplicateMigrationVersionException.cs
@@ -1,7 +1,14 @@
 ﻿namespace Kot.MongoDB.Migrations.Exceptions
 {
+    /// <summary>
+    /// Represents an error caused by existence of several migrations that have the same version.
+    /// </summary>
     public class DuplicateMigrationVersionException : MongoDbMigrationsException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DuplicateMigrationVersionException"/> class with a specified version.
+        /// </summary>
+        /// <param name="version">Migration version.</param>
         public DuplicateMigrationVersionException(DatabaseVersion version)
             : base($"There are several migrations with the same version: {version}.")
         {
