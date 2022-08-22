99fb9fa0650e172bd54d026b1e131b9ca2b74351
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-22 21:08:40 +0300

    Add comments for MigrationInstantiationException

--- Kot.MongoDB.Migrations/Exceptions/MigrationInstantiationException.cs
+++ Kot.MongoDB.Migrations/Exceptions/MigrationInstantiationException.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace Kot.MongoDB.Migrations.Exceptions
 {
+    /// <summary>
+    /// Represents a migration type instantiation error.
+    /// </summary>
     public class MigrationInstantiationException : MongoDbMigrationsException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MigrationInstantiationException"/> class with a specified migration type
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="migrationType">Migration type that failed to be instantiated.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception or a null reference
+        /// if no inner exception is specified.</param>
         public MigrationInstantiationException(Type migrationType, Exception innerException)
             : base($"Failed to instantiate migration class '{migrationType.FullName}'.", innerException)
         {
