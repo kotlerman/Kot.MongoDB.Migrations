7143a56a38a9fbc42098f80bd3ca3ba94725aa7e
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-22 21:48:13 +0300

    Add comments for IMigrationInstantiator

--- Kot.MongoDB.Migrations/IMigrationInstantiator.cs
+++ Kot.MongoDB.Migrations/IMigrationInstantiator.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace Kot.MongoDB.Migrations
 {
+    /// <summary>
+    /// Defines Mongo migrations instantiation method.
+    /// </summary>
     public interface IMigrationInstantiator
     {
+        /// <summary>
+        /// Create an instance of a specified migration type.
+        /// </summary>
+        /// <param name="migrationType">Migration type.</param>
+        /// <returns>An instance of the specified migration type.</returns>
         IMongoMigration Instantiate(Type migrationType);
     }
 }
