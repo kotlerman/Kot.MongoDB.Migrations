17fccfaddc879bd663f8dd5e02a713fdb275e74c
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-22 19:40:51 +0300

    Add comments for MigrationHistory

--- Kot.MongoDB.Migrations/MigrationHistory.cs
+++ Kot.MongoDB.Migrations/MigrationHistory.cs
@@ -5,15 +5,30 @@ using System;
 
 namespace Kot.MongoDB.Migrations
 {
+    /// <summary>
+    /// Represents a migration history record.
+    /// </summary>
     public class MigrationHistory
     {
+        /// <summary>
+        /// Migration history record identifier.
+        /// </summary>
         public ObjectId Id { get; set; }
 
+        /// <summary>
+        /// Version that the database was updated to as the migration was applied.
+        /// </summary>
         [BsonSerializer(typeof(DatabaseVersionSerializer))]
         public DatabaseVersion Version { get; set; }
 
+        /// <summary>
+        /// Migration name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Time the migration was applied at.
+        /// </summary>
         public DateTime AppliedAt { get; set; }
     }
 }
