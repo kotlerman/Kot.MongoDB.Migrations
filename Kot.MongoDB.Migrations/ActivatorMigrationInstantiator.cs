2d0b842b997d60f5fd03578d9b271f2319f979d2
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-22 21:49:38 +0300

    Add comments for ActivatorMigrationInstantiator

--- Kot.MongoDB.Migrations/ActivatorMigrationInstantiator.cs
+++ Kot.MongoDB.Migrations/ActivatorMigrationInstantiator.cs
@@ -5,8 +5,13 @@ using System.Text;
 
 namespace Kot.MongoDB.Migrations
 {
+    /// <summary>
+    /// Migration instantiator that creates instances using <see cref="Activator"/>.
+    /// </summary>
     public class ActivatorMigrationInstantiator : IMigrationInstantiator
     {
+        /// <inheritdoc/>
+        /// <exception cref="MigrationInstantiationException">Failed to create an instance of the specified migration type.</exception>
         public IMongoMigration Instantiate(Type migrationType)
         {
             try
