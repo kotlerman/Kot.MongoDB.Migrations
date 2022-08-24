008764b00220f89ae636274b6413bcfa63370e9c
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-24 11:20:19 +0300

    Add comments for IMigrationsLocator

--- Kot.MongoDB.Migrations/Locators/IMigrationsLocator.cs
+++ Kot.MongoDB.Migrations/Locators/IMigrationsLocator.cs
@@ -2,8 +2,15 @@
 
 namespace Kot.MongoDB.Migrations.Locators
 {
+    /// <summary>
+    /// Defines Mongo migrations loading method.
+    /// </summary>
     public interface IMigrationsLocator
     {
+        /// <summary>
+        /// Load migrations.
+        /// </summary>
+        /// <returns>An enumerable collection of migrations.</returns>
         IEnumerable<IMongoMigration> Locate();
     }
 }
