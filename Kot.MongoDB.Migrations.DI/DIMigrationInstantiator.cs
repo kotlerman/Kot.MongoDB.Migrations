551fd17c4eb8beb86cefba9535ee266af0c0dd82
kotlerman <3135083+kotlerman@users.noreply.github.com>
2022-08-25 22:53:36 +0300

    Add comments for DIMigrationInstantiator

--- Kot.MongoDB.Migrations.DI/DIMigrationInstantiator.cs
+++ Kot.MongoDB.Migrations.DI/DIMigrationInstantiator.cs
@@ -4,15 +4,25 @@ using System;
 
 namespace Kot.MongoDB.Migrations.DI
 {
+    /// <summary>
+    /// Migration instantiator that creates instances using <see cref="IServiceProvider"/>.
+    /// </summary>
     public class DIMigrationInstantiator : IMigrationInstantiator
     {
         private readonly IServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="DIMigrationInstantiator"/> that creates instances
+        /// using <paramref name="serviceProvider"/>.
+        /// </summary>
+        /// <param name="serviceProvider">A service provider used to resolve dependencies of migration types.</param>
         public DIMigrationInstantiator(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
+        /// <inheritdoc/>
+        /// <exception cref="MigrationInstantiationException">Failed to create an instance of the specified migration type.</exception>
         public IMongoMigration Instantiate(Type migrationType)
         {
             try
